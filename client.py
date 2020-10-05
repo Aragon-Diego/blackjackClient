ca4477b0975de41d501f74a04e0afffa1c00d84e
Diego Alonso Aragón Villarreal <32529071+Aragon-Diego@users.noreply.github.com>
2020-10-05 16:13:06 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,24 +1,73 @@
-import threading
+import socket
+import os
 import time
-from random import randint
-palo=["corazon","Trebol","Diamante","Espada"]
-numero={'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':10,'Q':10,'K':10,'A':1}
-turno=False
-def server():
-    global turno
-    while True:
-        n=randint(1,100)
-        print(n)
-        if(n>70):
-            turno=not turno
-        time.sleep(1)
-server = threading.Thread(target=server)
-server.start()
-print(not turno)
-count=0
-while(True):
-    while(not turno):
-        print("dentro del loop")
-        time.sleep(1)
-    print("ya tengo turno")
-    time.sleep(1)
+HOST = "172.32.151.204"  # The server's hostname or IP address
+PORT = 8080        # The port used by the server
+
+
+def elegir(arr):
+    decision="hold"
+    menos=0
+    mas=0
+    for i in arr:
+        if i==21:
+            mas=10000
+        elif i >16 and i<22:
+            mas+=1
+        elif i >21:
+            continue
+        else:
+            menos+=1
+    if mas<menos:
+        decision="call"
+    return decision
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    arr=[0]
+    cartas=[]
+    pregunta=False
+    jugador=""
+    while(True):
+        mensaje2=""
+        response_data = s.recv(1024).decode("utf-8") 
+        if "kill" in response_data:
+            break
+        elif "?" in response_data:
+            pregunta=True
+        elif len(response_data)<4:
+            cartas.append(response_data[:-1])
+            if response_data[0] in ['J','K','Q']:
+                for i in range(len(arr)):
+                    arr[i]+=10
+            elif response_data[0] == 'A':
+                arrA=[]
+                for i in range(len(arr)):
+                    arrA.append(arr[i]+1)
+                    arr[i]+=11
+                arr=arr+arrA
+            else:
+                for i in range(len(arr)):
+                    st=response_data[:-1]
+                    arr[i]+=int(st)
+        else:
+            if("Eres el Jugador" in response_data):
+               jugador=response_data
+            else:
+                mensaje2=response_data
+        if os.name=='nt':
+            os.system('cls')    
+        else:
+            os.system('clear')
+        print(jugador)
+        print(mensaje2)
+        print(cartas)
+        print(arr)
+        if(pregunta):
+            print("¿Quieres hold o call?")
+            mensaje = elegir(arr)
+            print(mensaje)
+            time.sleep(0.5)
+            s.sendall(bytes(mensaje, 'utf-8'))
+            pregunta=False
+    s.close()
